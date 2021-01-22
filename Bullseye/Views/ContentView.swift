73499bb2d51f756543a7be5170e5375bbf4d627f
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-01-22 11:25:58 -0800

    change background color to gray and accent color to red based on design specs

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,33 +14,44 @@ struct ContentView: View {
   @State private var game = Game()
   
   var body: some View {
-    VStack {
-      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-        .bold()
-        .kerning(2.0)
-        .multilineTextAlignment(.center)
-        .lineSpacing(4.0)
-        .font(.footnote)
-      Text(String(game.target))
-        .fontWeight(.black)
-        .font(.largeTitle)
-        .kerning(-1.0)
-      HStack {
-        Text("1")
+    ZStack {
+      Color("BackgroundColor")
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+      VStack {
+        Text("🎯🎯🎯\nPut the bullseye as close as you can to".uppercased())
           .bold()
-        Slider(value: $sliderValue, in: 1.0...100.0)
-        Text("100")
-          .bold()
+          .kerning(2.0)
+          .multilineTextAlignment(.center)
+          .lineSpacing(4.0)
+          .font(.footnote)
+        Text(String(game.target))
+          .fontWeight(.black)
+          .font(.largeTitle)
+          .kerning(-1.0)
+        HStack {
+          Text("1")
+            .bold()
+          Slider(value: $sliderValue, in: 1.0...100.0)
+          Text("100")
+            .bold()
+        }
+        .padding()
+        Button(action: {
+          alertIsVisible = true
+        }) {
+          Text("Hit Me".uppercased())
+            .bold()
+            .font(.title3)
+        }
+        .padding(20.0)
+        .background(Color.blue)
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+        .alert(isPresented: $alertIsVisible, content: {
+          let roundedValue = Int(sliderValue.rounded())
+          return Alert(title: Text("Hello there!"), message: Text("The sliders value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
+        })
       }
-      Button(action: {
-        alertIsVisible = true
-      }) {
-        Text("Hit Me")
-      }
-      .alert(isPresented: $alertIsVisible, content: {
-        let roundedValue = Int(sliderValue.rounded())
-        return Alert(title: Text("Hello there!"), message: Text("The sliders value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
-      })
     }
   }
 }
